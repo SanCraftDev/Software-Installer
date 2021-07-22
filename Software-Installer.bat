a3b54b9da15bc99b977afc68159ab3de75f67820
2020Sanoj <75573284+2020Sanoj@users.noreply.github.com>
2021-07-22 21:01:21 +0200

    Kategorien die erste

--- Software-Installer.bat
+++ Software-Installer.bat
@@ -8,3 +8,12 @@ del "%userprofile%\AppData\Local\Temp\Programm.bat"
 CLS
 echo.
 echo Wilkommen!
+echo 1. Browser/E-Mail/Office/E-Mail
+echo 2. Tools (Normale Nutzer)
+echo 3. Spiele
+echo 4. Social Media/Messenger
+echo 5. Tools (Richtung Programmierung)
+echo 6. Programmierung
+echo 7. Aufnahme Tools
+echo 8. Kreatives
+echo 9. Entertaiment
