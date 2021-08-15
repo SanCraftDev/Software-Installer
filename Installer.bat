10d17ab54a0b35befc4598cf22e23ccf468260a1
2020Sanoj <75573284+2020Sanoj@users.noreply.github.com>
2021-08-15 22:59:48 +0200

    Restart closes old window now after second restart too

--- Installer.bat
+++ Installer.bat
@@ -115,5 +115,5 @@ del /S /Q %0
 GOTO end
 
 :restart
-start %0
+start cmd.exe /C %0
 EXIT /B
--- Software-Installer.bat
+++ Software-Installer.bat
@@ -30,7 +30,7 @@ IF ERRORLEVEL 2 GOTO tl
 IF ERRORLEVEL 1 GOTO of
 
 :restart
-start %0
+start cmd.exe /C %0
 EXIT /B
 
 :end
